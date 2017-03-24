ebb62ce2958d46ca42858c8fb8a5dcb97d30c726
Jose Rojas <jose23rojas@unitec.edu>
2017-03-24 14:16:29 -0600

    clase Fruta del Diablo (DevilsFruit) TERMINADA

---
+++ DevilsFruit.cpp
@@ -0,0 +1,7 @@
+// Fruta del Diablo.cpp
+
+#include "DevilsFruit.h"
+
+DevilsFruit::DevilsFruit(string nombre) { this -> nombre = nombre; }
+
+string DevilsFruit::getNombre() { return nombre; }
---
+++ DevilsFruit.h
@@ -0,0 +1,20 @@
+// Fruta del Diablo.h
+
+#include <string>
+
+using namespace std;
+
+#ifndef DEVILSFRUIT_H
+#define DEVILSFRUIT_H
+
+class DevilsFruit
+{
+        protected:
+                string nombre;
+        public:
+                DevilsFruit(string);
+
+                string getNombre();
+};
+
+#endif
